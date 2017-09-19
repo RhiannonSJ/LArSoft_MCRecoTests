7aa0caf74257a00e3529907ccd7209da1281d431
Andrew Smith <andrewsmithweb@gmail.com>
2017-09-19 09:11:14 -0500

    Making sure git is up to date - errors may be present - pulling to v06_49_03

--- MCRecoComp_module.cc
+++ MCRecoComp_module.cc
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////
-// Class:       MCRecoComp
+// Class:	     MCRecoComp
 // Plugin Type: analyzer (art v2_07_03)
 // File:        MCRecoComp_module.cc
 //
@@ -19,47 +19,105 @@
 #include "canvas/Utilities/InputTag.h"
 #include "fhiclcpp/ParameterSet.h"
 #include "messagefacility/MessageLogger/MessageLogger.h"
+#include "art/Framework/Services/Registry/ServiceHandle.h"
+#include "art/Framework/Services/Optional/TFileService.h"
+#include "nusimdata/SimulationBase/MCTruth.h"
+#include "lardataobj/RecoBase/Hit.h"
+#include "lardataobj/RecoBase/Vertex.h"
+#include "lardataobj/MCBase/MCTrack.h"
+#include "larreco/RecoAlg/PMAlg/PmaTrack3D.h"
 
 namespace recotests {
-  class MCRecoComp;
+	class MCRecoComp;
 }
 
 
 class recotests::MCRecoComp : public art::EDAnalyzer {
 public:
-  explicit MCRecoComp(fhicl::ParameterSet const & p);
-  // The compiler-generated destructor is fine for non-base
-  // classes without bare pointers or other resource use.
+	explicit MCRecoComp(fhicl::ParameterSet const & p);
+	// The compiler-generated destructor is fine for non-base
+	// classes without bare pointers or other resource use.
 
-  // Plugins should not be copied or assigned.
-  MCRecoComp(MCRecoComp const &) = delete;
-  MCRecoComp(MCRecoComp &&) = delete;
-  MCRecoComp & operator = (MCRecoComp const &) = delete;
-  MCRecoComp & operator = (MCRecoComp &&) = delete;
+	// Plugins should not be copied or assigned.
+	MCRecoComp(MCRecoComp const &) = delete;
+	MCRecoComp(MCRecoComp &&) = delete;
+	MCRecoComp & operator = (MCRecoComp const &) = delete;
+	MCRecoComp & operator = (MCRecoComp &&) = delete;
 
-  // Required functions.
-  void analyze(art::Event const & e) override;
+	// Required functions.
+	void analyze(art::Event const & e) override;
 
-  // Selected optional functions.
-  void reconfigure(fhicl::ParameterSet const & p) override;
+	// Selected optional functions.
+	void reconfigure(fhicl::ParameterSet const & p) override;
 
 private:
 
-  // Declare member data here.
+	// Declare member data here.
 
 };
 
 
-recotests::MCRecoComp::MCRecoComp(fhicl::ParameterSet const & p)
-  :
-  EDAnalyzer(p)  // ,
- // More initializers here.
+recotests::MCRecoComp::MCRecoComp(fhicl::ParameterSet const & p):
+    EDAnalyzer(p)// ,
+    // More initializers here.
 {}
+
 
 void recotests::MCRecoComp::analyze(art::Event const & e)
 {
-  //
-  // Comparing
+	//
+	// Comparing truth and reco information
+	// 
+	// Start by printing event info to a txt file
+	
+	// Get the truth information
+	// Get the hit handle
+	art::Handle< std::vector< recob::Hit > > hit_handle;
+	e.getByLabel("gaushit", hit_handle );
+ 
+	// Get the truth handle
+	art::Handle< std::vector< simb::MCTruth > > mct_handle;
+	e.getByLabel( "generator", mct_handle );
+
+	// Get the truth track handle
+	art::Handle< std::vector< sim::MCTrack > > mctrack_handle;
+	e.getByLabel( "pmalgtrackmaker", mctrack_handle );
+
+	// Get the 3D track information
+	art::Handle< std::vector< pma::Track3D > > track3d_handle;
+	e.getByLabel("pmalgtrackmaker", track3d_handle );
+
+	// Check validity of the handle
+	int mctrack_size = mctrack_handle->size();
+	int mct_size     = mct_handle->size();
+
+    // MCTrack
+	if( mctrack_handle.isValid() && mctrack_size ){
+
+        // Let's start by simply printing out some information
+        for ( auto const& mctrack : (*mctrack_handle)){
+    
+            std::cout << "'Reco?' PDG Codes : " << mctrack.PdgCode() << std::endl; 
+            std::cout << "'Reco?' Track ID  : " << mctrack.TrackID() << std::endl; 
+
+        }
+
+    }
+
+    // Truth stuff
+    if( mct_handle.isValid() && mct_size ){
+
+        // Let's start by simply printing out some information
+        for ( auto const& mct : (*mct_handle)){
+   
+            int n_particles = mct.NParticles();
+            for ( int i = 0; i < n_particles; ++i ){
+                std::cout << "Truth PDG Codes  : " << mct.GetParticle(i).PdgCode() << std::endl; 
+                std::cout << "Truth Track ID   : " << mct.GetParticle(i).TrackId() << std::endl; 
+
+            }
+        }
+    }
 }
 
 void recotests::MCRecoComp::reconfigure(fhicl::ParameterSet const & /*p*/)
--- test/run_mcrecocomp.sh
+++ test/run_mcrecocomp.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-INPUT_FILE=/sbnd/data/users/rsjones/cczeropi_outputs/cczeropi_events_rerun_reco_1.root
+INPUT_FILE=/sbnd/data/users/rsjones/hit_filtered_cc0pi_sample/19366014_12/prodgenie_sbnd_GenieGen-20170808T233908_5dd110c9-c622-4eb5-b5ed-8b800d0cb0d5_topologyFilteredEvents_TopologyFilter-20170809T000505_G4-20170809T012154__b7bc1705-c469-4b40-940c-eb5e548df128.root
+
 N_EVENTS=10
 
+WORKING_DIR=/sbnd/app/users/rsjones/LArSoft_reco_validation/LArSoft-v06_45_00/srcs/recoperformance/recoperformance/working_dir
+
+cd $WORKING_DIR
+
 lar -c run_mcRecoComp.fcl -s ${INPUT_FILE} -n ${N_EVENTS}
+
+cd -
